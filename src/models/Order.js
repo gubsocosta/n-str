0ad9ab3b70a7ffa02ce8f3fa66f0ba15f4c58835
Gabriel Costa <gubsocosta@gmail.com>
2020-02-17 16:02:25 -0300

    create order model

---
+++ src/models/Order.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    number: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['created', 'done'],
+        default: 'created'
+    },
+    customerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Customer'
+    },
+    items: [{
+        qunatity: {
+            type: Number,
+            required: true,
+            default: 1,
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    }],
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
