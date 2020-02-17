90e6d670a75ac04243b7e286277546e62b0b4a7b
Gabriel Costa <gubsocosta@gmail.com>
2020-02-17 15:46:47 -0300

    create customer model

---
+++ src/models/Customer.js
@@ -0,0 +1,22 @@
+'use restrict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    paswword: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('Customer', CustomerSchema);
